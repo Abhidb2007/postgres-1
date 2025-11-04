fa4eaa6bb0edf463112b21bc1e303361757545b9
Abhidb2007 <abhishekdb2210@gmail.com>
2025-11-04 23:18:10 +0530

    code

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,20 @@
 import { Client } from 'pg';
+import * as express from 'express';
+
+const app = express();
+app.use(express.json());
 
 const pgClient = new Client('postgresql://neondb_owner:npg_7LTawG5Usrjx@ep-aged-violet-adh5o17p-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require');
 
-pgClient.connect()
-app.post("/signup",async(req, res)=>{
-    const username=req.body.username;
-    const password=req.body.password;
-    const email=req.body.email;
-     let sqlQuery="INSERT INTO users(username,password,email) VALUES ("
-     sqlQuery+=username;
-     sqlQuery+=",";
-     sqlQuery+=email;
-     sqlQuery+=",";
-     sqlQuery+=password;
-     sqlQuery+=");"
-    const response=await pgClient.query("INSERT INTO users (username,email.password) VALUES (${username},${email},${password});");
+pgClient.connect();
+app.post("/signup", async (req: express.Request, res: express.Response) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+    const insertQuery = `INSERT INTO users (username, password, email) VALUES ('${username}', '${password}', '${email}')`;
+    const response = await pgClient.query(insertQuery);
     res.json({
-        message:"User signed up successfully"
+        message: "User signed up successfully"
     })
 })
-export default pgClient
+app.listen(3000);
